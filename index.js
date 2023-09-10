108388c6eb68d3141cefa305fd686479cb8a6bac
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-09-09 22:30:19 -0600

    Serve index.html

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+	res.setHeader("Content-Type", "text/html");
 
-server.listen("localhost", 3000, () => {
+	fs.readFile("./views/index.html", (err, data) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.write(data);
+		}
+		res.end();
+	});
+});
+
+server.listen(3000, "localhost", () => {
 	console.log("listening on port 3000");
 });
