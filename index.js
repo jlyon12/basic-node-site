5caea1f9cd0072ca678403dcc2f8b3170f02767c
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-09-09 23:00:43 -0600

    Create router

--- index.js
+++ index.js
@@ -4,7 +4,27 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
 	res.setHeader("Content-Type", "text/html");
 
-	fs.readFile("./views/index.html", (err, data) => {
+	let path = "./views/";
+
+	switch (req.url) {
+		case "/":
+			path += "index.html";
+			res.statusCode = 200;
+			break;
+		case "/about":
+			path += "about.html";
+			res.statusCode = 200;
+			break;
+		case "/contact":
+			path += "contact.html";
+			res.statusCode = 200;
+			break;
+		default:
+			path += "404.html";
+			res.statusCode = 404;
+			break;
+	}
+	fs.readFile(path, (err, data) => {
 		if (err) {
 			console.log(err);
 		} else {
