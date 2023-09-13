0f9da681092273e9770c1d97a99f102f68129f6a
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-09-12 20:35:37 -0600

    Refactor to use express

--- index.js
+++ index.js
@@ -1,39 +1,20 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
 
-const server = http.createServer((req, res) => {
-	res.setHeader("Content-Type", "text/html");
-
-	let path = "./views/";
-
-	switch (req.url) {
-		case "/":
-			path += "index.html";
-			res.statusCode = 200;
-			break;
-		case "/about":
-			path += "about.html";
-			res.statusCode = 200;
-			break;
-		case "/contact":
-			path += "contact.html";
-			res.statusCode = 200;
-			break;
-		default:
-			path += "404.html";
-			res.statusCode = 404;
-			break;
-	}
-	fs.readFile(path, (err, data) => {
-		if (err) {
-			console.log(err);
-		} else {
-			res.write(data);
-		}
-		res.end();
-	});
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "./views/", "index.html"));
+});
+app.get("/about", (req, res) => {
+	res.sendFile(path.join(__dirname, "./views/", "about.html"));
+});
+app.get("/contact", (req, res) => {
+	res.sendFile(path.join(__dirname, "./views/", "contact.html"));
+});
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "./views/", "404.html"));
 });
 
-server.listen(3000, "localhost", () => {
-	console.log("listening on port 3000");
-});
+app.listen(port);
+console.log(`Serving on port ${port}`);
